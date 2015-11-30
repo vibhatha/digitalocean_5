00bbdc4a02f13d30b0a7c1170c190ff3efb4b197
vibhatha <vibhatha@gmail.com>
2015-11-30 23:01:18 +0530

    remove files

--- scripts/writeX1.py
+++
@@ -1,9 +0,0 @@
-
-def write(text):
-    fo = open("X.txt", "a")
-    #fo.write(bytes(text, 'UTF-8'))
-    #fo.write(bytes('\n', 'UTF-8'))
-    fo.write(text)
-    fo.write('\n')
-    # Close opend file
-    fo.close()
